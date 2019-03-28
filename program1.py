2e7e222d3f1def2f8dcaacac78057608cd77d503
Sandeep Rawat <sandy4u1994@gmail.com>
2019-03-28 22:52:47 +0530

    login signup updated

--- program1.py
+++ program1.py
@@ -1,18 +1,21 @@
+usrList = []
+pasList = []
 print("Welcome to Login/Signup Program")
 
 ans = input("Do you have an account? please enter y/n:")
 
 if ans == 'n':
     usr = input("Please Create Username: ")
+    usrList.append(usr)
     pas = input("Please Create Password: ")
-
+    pasList.append(pas)
     print("Congrulation, Your account created")
 
 else:
     print("Please Login Here:")
     usrinput = input("Please Enter Your Username: ")
     pasinput = input("Please Enter Your Password: ")
-    if usr == usrinput and pas == pasinput:
+    if usrinput == (usrList) and pasinput == (pasList):
         print("Welcome to My World: ")
 
     else:
